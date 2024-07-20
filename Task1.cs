5a6bb96859f64caf86e97f8c63ce59fd09180a90
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2024-07-21 01:34:41 +0200

    Add files via upload
    The program adds two integers of the array and compares its sum to the given number and determines if the two numbers are equal or not

---
+++ Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {2,3, 4, 5, 6};
+            int sum = 0;
+            int target = 11;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                sum = numbers[i] + numbers[i]+1;
+                if(sum==target)
+                {
+                    Console.WriteLine($"{numbers[i]} and {numbers[i] + 1} equals to {sum}");
+                    break;
+                }
+                else { Console.WriteLine($"{numbers[i]} and {numbers[i] + 1} is not equals to {target}"); }
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
