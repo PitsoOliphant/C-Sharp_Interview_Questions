8b70fbc6cdc6b3891cecaee3a7399680200f4327
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2024-08-01 15:45:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greatestValue = 0;
+            int[] numbers = { 2, 5, 1, 22, 0 };
+            int temp;
+            for (int i = 0; i < numbers.Length-1; i++) 
+            {
+                for (int j = i+1;j<numbers.Length;j++) 
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+
+
+                }
+            }
+            
+            foreach (int i in numbers) 
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
