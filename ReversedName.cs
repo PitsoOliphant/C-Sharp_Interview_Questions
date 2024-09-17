f98c991fa49c2f44513ad7efc3eca5c21b21ea4e
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2024-09-17 07:52:45 -0700

    Add files via upload
    C# program that automatically reverses the name "Pitso" and enforces proper punctuation on the reversed name, by assigning a capital letter to it

---
+++ ReversedName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "pitso";
+            string reversedName = "";
+
+            
+            for (int i = name.Length-1; i >=0; i--)
+            {
+                reversedName += name[i];
+               
+            }
+
+           Console.WriteLine(reversedName = char.ToUpper(reversedName[0]) + reversedName.Substring(1));
+           Console.ReadLine();
+        
+             
+        }
+    }
+}
