0a50893525b4033e67a75a41ffc08d2fb0e87bcf
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2024-09-18 01:57:03 -0700

    Add files via upload
    C# program that uses a function to determine whether a word entered by the is Palindrome or not. If it is it returns a true statement, else it returns a false statement to the user.

---
+++ Palindromes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    internal class Program
+    {
+        static bool Palindrome(string input)
+        {
+            bool result=true;
+            string palindrom = "";
+
+            for (int j = input.Length - 1; j >= 0; j--)
+            {
+
+                palindrom += input[j];
+                
+
+            }
+           
+            for(int i =0;i< palindrom.Length;i++)
+            {
+                if(input[i] == palindrom[i]) result = true ;
+                else result = false;
+            }
+            
+            
+                return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Palindrome");
+            string UserInput =Console.ReadLine();
+
+            Console.WriteLine(Palindrome(UserInput[0].ToString().ToLower()+UserInput.Substring(1)));
+
+            Console.ReadLine();
+
+        }
+    }
+}
