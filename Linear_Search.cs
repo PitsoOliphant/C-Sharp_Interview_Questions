dadd445ddaa066f4f9891049a404143aa7f025b1
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2024-09-18 04:06:36 -0700

    Add files via upload
    C# program that matches user input to existing set of data in an array. If values entered match it displays the value, if not it tells the user that the values do not exist

---
+++ Linear_Search.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            string[] name = { "112", "001", "10" };
+            string userInput =Console.ReadLine();
+            for(int i =0;i< name.Length;i++)
+            {
+                if (userInput == name[i])
+                {
+                    Console.WriteLine(name[i]);
+                    break;
+                }
+                else
+                {
+                     Console.WriteLine("Does not exist");
+                    break;
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+    
+
+ }
+
+    
+
